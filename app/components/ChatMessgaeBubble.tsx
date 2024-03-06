ff9137d495001764abac83f70f629d283c1f117f
Ayush Saklani <saklaniayush99@gmail.com>
2024-03-06 02:42:52 -0500

    intinal chat ui

---
+++ app/components/ChatMessgaeBubble.tsx
@@ -0,0 +1,31 @@
+import type { Message } from "ai/react";
+
+export function ChatMessageBubble(props: { message: Message, aiEmoji?: string, sources: any[] }) {
+  const colorClassName =
+    props.message.role === "user" ? "bg-sky-600" : "bg-slate-50 text-black";
+  const alignmentClassName =
+    props.message.role === "user" ? "ml-auto" : "mr-auto";
+  const prefix = props.message.role === "user" ? "🧑" : props.aiEmoji;
+  return (
+
+    <div
+      className={`${alignmentClassName} ${colorClassName} rounded px-4 py-2 max-w-[80%] mb-8 flex`}
+    >
+      {
+       (props.message.role == "assistant") && (
+     <div className="mr-2">
+        {prefix}
+      </div>)
+      } 
+      <div className="whitespace-pre-wrap flex flex-col">
+        <span>{props.message.content}</span>
+      </div>
+      {
+       (props.message.role === "user") && (
+     <div className="ml-2">
+        {prefix}
+      </div>)
+      } 
+    </div>
+  );
+}
